5f6a8440f42bd88d6574abcf1dae0cd705263913
Tina Wang <tina@tina.cafe>
2021-09-30 23:32:34 +0200

    ⚡ Cache albums list

--- store/library.js
+++ store/library.js
@@ -17,6 +17,21 @@ export const mutations = {
 }
 
 export const actions = {
+    async syncAlbums({ state, commit }) {
+        // FIXME: Called twice ?
+
+        let ids = await this.$http.$get(`${process.env.BACKEND_URL}/library/albums_ids`);
+        // 💡 if diff <= 3 we can cherry pick missing ones.
+        if (ids - state.albums.length <= 3) {
+            // TODO: get album/album_id
+        }
+        else {
+            let albums = await this.$http.$get(`${process.env.BACKEND_URL}/library/albums`);
+            albums = albums.filter(album => state.albums.find(cached_album => cached_album._id != album._id));
+            albums.forEach(album => commit('cacheAlbum', album));
+        }
+    },
+
     async retrieveAlbum({ commit }, album_id) {
         let album = await this.$http.$get(`${process.env.BACKEND_URL}/library/album/${album_id}`);
         commit('cacheAlbum', album);
@@ -24,10 +39,13 @@ export const actions = {
 }
 
 export const getters = {
+    albums: (state) => {
+        return state.albums;
+     },
     current_track: (state, getters) => {
         return getters.current_album.track_list?.find(track => track._id === state.current_track_id) || {};
     },
-    current_album: state => {
+    current_album: (state) => {
         return state.albums.find(album => album._id === state.current_album_id) || {};
     }
 }
