ce311141717678b217404a6040834634331ed3cd
Tina Wang <tina@tina.cafe>
2021-10-05 10:34:31 +0200

    🐛 Prevent modal closing when clicking on player

--- components/FloatingPlayer.vue
+++ components/FloatingPlayer.vue
@@ -1,13 +1,13 @@
 <template>
-  <div v-if="track._id" class="player_container">
+  <div ref="ply" v-if="track._id" class="floating_player_container">
     <div class="relative h-full w-full p-2 flex justify-between">
       <div class="flex flex-col justify-between">
         <h4 class="font-black leading-none">{{track.title}}</h4>
         <h4 class="font-semibold">{{album.artist_name}}</h4>
       </div>
       <ui-button icon="play" @click.native="play" />
-      <div class="player__right_lip"></div>
-      <div class="player__bottom_lip"></div>
+      <div class="floating_player__right_lip"></div>
+      <div class="floating_player__bottom_lip"></div>
     </div>
   </div>
 </template>
@@ -32,19 +32,19 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
-.player_container {
+.floating_player_container {
   @apply fixed bottom-12 right-12 z-50;
   @apply w-64 lg:w-96 h-18 lg:h-24;
   @apply border border-current bg-brand-secondary;
   @apply shadow-xl;
 }
 
-.player__right_lip {
+.floating_player__right_lip {
   @apply absolute top-3 -right-3 lg:top-4 lg:-right-4;
   @apply h-full w-3 lg:w-4;
   @apply bg-brand-primary;
 }
-.player__bottom_lip {
+.floating_player__bottom_lip {
   @apply absolute -bottom-3 -right-3 lg:-bottom-4 lg:-right-4;
   @apply h-3 lg:h-4 w-full;
   @apply bg-brand-primary;
--- plugins/directives/vue-click-outside.js
+++ plugins/directives/vue-click-outside.js
@@ -4,7 +4,7 @@ Vue.directive('click-outside', {
   bind: function (el, binding, vnode) {
     el.clickOutsideEvent = function (event) {
       // here I check that click was outside the el and his children
-      if (!(el == event.target || el.contains(event.target))) {
+      if (!(el == event.target || el.contains(event.target) || event.target.closest('.floating_player_container'))) {
         // and if it did, call method provided in attribute value
         vnode.context[binding.expression](event);
       }
